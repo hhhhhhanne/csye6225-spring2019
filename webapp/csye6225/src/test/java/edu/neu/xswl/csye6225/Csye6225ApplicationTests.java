0330506134c0d53010743af39cc52dbf5b228b0c
Xiaohan Zhao <zhao.xiaoh@husky.neu.edu>
2019-01-29 20:42:14 -0500

    project demo

---
+++ webapp/csye6225/src/test/java/edu/neu/xswl/csye6225/Csye6225ApplicationTests.java
@@ -0,0 +1,17 @@
+package edu.neu.xswl.csye6225;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Csye6225ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
