937a104dc7dab53a4f2c44421b24fb5fb3fa4e11
AngLi2 <31733648+AngLi2@users.noreply.github.com>
2019-01-31 13:06:23 -0500

    Update Email Validation

---
+++ webapp/csye6225/src/main/java/edu/neu/xswl/csye6225/utils/EmailValidationUtil.java
@@ -0,0 +1,9 @@
+package edu.neu.xswl.csye6225.utils;
+
+public interface EmailValidationUtil {
+
+    /*
+    Validate if the input is a valid email
+     */
+    public boolean isEmail(String str);
+}
