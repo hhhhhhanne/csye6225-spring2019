1410b20730c2f9d9f652d6da480c46b14f682395
AngLi2 <31733648+AngLi2@users.noreply.github.com>
2019-02-05 22:22:27 -0500

    Update aws setup and teardown files

--- infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 
-echo Deleting vpc, below is the Vpcs:
+echo Deleting networking resources using AWS CLI ...
 aws ec2 describe-vpcs --query 'Vpcs[*].{VpcId:VpcId,Default:IsDefault}'
 
-read -p "Please enter the VpcId you want to delete: " vpcId
+read -p "Enter the Vpc Id to be deleted: " vpcId
 
 routeTable1=`aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcId" --query 'RouteTables[0].RouteTableId' --output text`
 routeTable2=`aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcId" --query 'RouteTables[1].RouteTableId' --output text`
 
 aws ec2 describe-route-tables
 
-read -p "Please enter the RoutetableId you want to delete: " routeTable2
+read -p "Enter the Routetable Id to be deleted: " routeTable2
 
-echo Start to delete Route Table $routeTable2 associated with $vpcId ...
+echo Deleting Route Table $routeTable2 with $vpcId ...
 aws ec2 delete-route-table --route-table-id $routeTable2
 
 
 internetGateway=`aws ec2 describe-internet-gateways --filters "Name=attachment.vpc-id,Values=$vpcId" --query 'InternetGateways[*].InternetGatewayId' --output text`
-echo Start to delete Internet Gateway $internetGateway associated with $vpcId ...
+echo Deleting Internet Gateway $internetGateway with $vpcId ...
 aws ec2 detach-internet-gateway --internet-gateway-id $internetGateway --vpc-id $vpcId
 aws ec2 delete-internet-gateway --internet-gateway-id $internetGateway
 
 aws ec2 delete-vpc --vpc-id $vpcId
-echo Vpc $vpcId is deleted!
+echo Vpc $vpcId is successfully deleted!
