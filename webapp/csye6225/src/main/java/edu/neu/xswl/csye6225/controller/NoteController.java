c795bca95d60b532bf7caea81c453bec55a974d7
AngLi2 <31733648+AngLi2@users.noreply.github.com>
2019-02-10 19:22:16 -0500

    Add simple controller

--- webapp/csye6225/src/main/java/edu/neu/xswl/csye6225/controller/NoteController.java
+++ webapp/csye6225/src/main/java/edu/neu/xswl/csye6225/controller/NoteController.java
@@ -3,9 +3,18 @@ package edu.neu.xswl.csye6225.controller;
 import edu.neu.xswl.csye6225.service.NoteService;
 import edu.neu.xswl.csye6225.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
+@RequestMapping("/note")
 public class NoteController {
 
     @Autowired
@@ -13,6 +22,13 @@ public class NoteController {
 
     @Autowired
     NoteService noteService;
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<Map<String, String>> getAllNotes(){
+        Map<String, String> messageMap = new HashMap<>();
+        messageMap.put("goushi", "dagoushi");
+        return new ResponseEntity<>(messageMap, HttpStatus.OK);
+    }
 
 
 }
