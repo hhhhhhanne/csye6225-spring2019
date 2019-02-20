04e8ab8ef5e64684fde1588bb6a5a7def07ad721
Chancelife <liu.changsi@husky.neu.edu>
2019-02-20 17:02:57 -0500

    add cloudformation json and shell. Complete sbunet and security part.

---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -0,0 +1,4 @@
+echo "Enter your stack name:"
+read name
+echo "your stack name is :" $name
+aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-application.json --parameters "ParameterKey=StackName,ParameterValue=$name"
