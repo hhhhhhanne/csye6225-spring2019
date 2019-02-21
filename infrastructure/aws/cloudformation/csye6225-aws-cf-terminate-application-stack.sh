b404c9324dcf46a6487607fc46175de7baf90326
Chancelife <liu.changsi@husky.neu.edu>
2019-02-20 20:16:01 -0500

    add cloudformation wait to create and terminate shell

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-application-stack.sh
@@ -1,3 +1,5 @@
 echo "Enter your stack name: "
 read name
 aws cloudformation delete-stack --stack-name $name
+aws cloudformation stack-delete-complete --stack0name $name
+echo "Stack $name deleted!"
