0c1c9357fb86202e7d13959c0f56ae171c136d15
Chancelife <liu.changsi@husky.neu.edu>
2019-03-12 23:53:43 -0400

    modify networking and application cloudformation template

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -2,3 +2,4 @@ echo "Enter your stack name:"
 read name
 echo "your stack name is :" $name
 aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-networking.json --parameters "ParameterKey=StackName,ParameterValue=$name"
+aws cloudformation wait stack-create-complete --stack-name $name
--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -1,3 +1,4 @@
 echo "Enter your stack name: "
 read name
 aws cloudformation delete-stack --stack-name $name
+aws cloudformation wait stack-delete-complete --stack-name $name
