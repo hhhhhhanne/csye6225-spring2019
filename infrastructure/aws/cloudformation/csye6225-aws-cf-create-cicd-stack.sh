5ccc4a9b84bed753114edc7f86ab372f84310571
Chancelife <liu.changsi@husky.neu.edu>
2019-03-14 14:35:40 -0400

    modify policy. not is face the assignment5 requirement. remove hardcoded s3 bucket name

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -2,7 +2,11 @@ echo "Enter your CI/CD stack name:"
 read name
 echo "your CI/CD stack name is :" $name
 
-aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-cicd.json --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=StackName,ParameterValue=$name
+echo "Enter your name for S3 bucket:"
+read bucketname
+echo "your CodeDeploy S3 bucket is: code-deploy.csye6225-spring2019-"$bucketname".me"
+
+aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-cicd.json --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=StackName,ParameterValue=$name ParameterKey=BucketName,ParameterValue=$bucketname
 
 aws cloudformation wait stack-create-complete --stack-name $name
 echo "Stack $name successfully created"
