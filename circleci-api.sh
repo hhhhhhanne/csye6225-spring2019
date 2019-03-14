54e89c6fd84009fabde4a748ecfdfd935ede89a8
Junjie He <he.jun@husky.neu.edu>
2019-03-13 22:49:42 -0400

    5

---
+++ circleci-api.sh
@@ -0,0 +1,3 @@
+curl -u 8242be22d5be7127a7bb03addfba43341f6b9b06 \
+     -d build_parameters[CIRCLE_JOB]=build \
+     https://circleci.com/api/v1.1/project/github/Junjie4025/csye6225-spring2019/tree/assignment5
