9e5c257b685561c302d022e4cfc99cf604f5205e
Xiaohan Zhao <zhao.xiaoh@husky.neu.edu>
2019-03-15 06:26:50 -0400

    again

--- infrastructure/aws/codedeploy/startJar.sh
+++ infrastructure/aws/codedeploy/startJar.sh
@@ -4,4 +4,4 @@ pwd
 cd ~/webapp/csye6225/target
 pwd
 ls
-nohup java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.cudakpsmqak8.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080 2>&1 &
+java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.cudakpsmqak8.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080 >/dev/null 2>&1 &
