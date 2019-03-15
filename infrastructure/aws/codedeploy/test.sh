89c603b98eec56a37de061da104d6dfd9d1be92e
Xiaohan Zhao <zhao.xiaoh@husky.neu.edu>
2019-03-15 01:14:59 -0400

    profile

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -5,4 +5,4 @@ cd ~
 sudo chmod +x csye6225-0.0.1-SNAPSHOT.jar
 mysql -h csye6225-spring2019.cudakpsmqak8.us-east-1.rds.amazonaws.com -ucsye6225master -p'csye6225password' < sql.sql
 exit
-nohup java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.active.profile=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.cudakpsmqak8.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080 &
+nohup java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.cudakpsmqak8.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080 &
