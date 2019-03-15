6f029a6da3940d2265089c8d0726d817fef0ee0d
Xiaohan Zhao <zhao.xiaoh@husky.neu.edu>
2019-03-15 01:49:42 -0400

    output

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -3,4 +3,4 @@ echo "start"
 sudo service tomcat stop
 cd ~
 sudo chmod +x csye6225-0.0.1-SNAPSHOT.jar
-java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.cudakpsmqak8.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080
+nohup java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.cudakpsmqak8.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080 &
