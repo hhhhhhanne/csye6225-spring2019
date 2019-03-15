a3d112c9dc15bc4c29b9de0e4b24077d405dc12e
Chancelife <liu.changsi@husky.neu.edu>
2019-03-14 22:07:22 -0400

    cd

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "start"
+cd ~
 chmod +x csye6225-0.0.1-SNAPSHOT.jar
 mysql -h csye6225-spring2019.ckwry3x5tnw1.us-east-1.rds.amazonaws.com -ucsye6225master -p'csye6225password' < sql.sql
 exit
