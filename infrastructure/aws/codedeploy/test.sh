c084b7cd3a6ed3e5c1d24a431ee898dd4ff766c0
Chancelife <liu.changsi@husky.neu.edu>
2019-03-14 22:36:43 -0400

    stop tomcat nohup

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "start"
+sudo service tomcat stop
 cd ~
 sudo chmod +x csye6225-0.0.1-SNAPSHOT.jar
 mysql -h csye6225-spring2019.ckwry3x5tnw1.us-east-1.rds.amazonaws.com -ucsye6225master -p'csye6225password' < sql.sql
 exit
-java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.active.profile=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.ckwry3x5tnw1.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080
+nohup java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.active.profile=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.ckwry3x5tnw1.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080 &
