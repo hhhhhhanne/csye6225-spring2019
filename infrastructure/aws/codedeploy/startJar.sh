ef8791deb93d19cf7421209e193bdbd6ba96054d
Xiaohan Zhao <zhao.xiaoh@husky.neu.edu>
2019-03-15 04:08:19 -0400

    have a try2

--- infrastructure/aws/codedeploy/startJar.sh
+++ infrastructure/aws/codedeploy/startJar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "start jar"
 cd ~
+ls
 sudo chmod +x csye6225-0.0.1-SNAPSHOT.jar
 java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.cudakpsmqak8.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080
