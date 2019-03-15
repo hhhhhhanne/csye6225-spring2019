124911b77b6d5028e30b9860fddb63ea6770d28c
Chancelife <liu.changsi@husky.neu.edu>
2019-03-14 22:33:57 -0400

    jdbc

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -4,4 +4,4 @@ cd ~
 sudo chmod +x csye6225-0.0.1-SNAPSHOT.jar
 mysql -h csye6225-spring2019.ckwry3x5tnw1.us-east-1.rds.amazonaws.com -ucsye6225master -p'csye6225password' < sql.sql
 exit
-java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.active.profile=dev --spring.datasource.url=csye6225-spring2019.ckwry3x5tnw1.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080
+java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.active.profile=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.ckwry3x5tnw1.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080
