4d597e04c0436a6cfbffc8ad7a6e6d8b734aa7d3
Xiaohan Zhao <zhao.xiaoh@husky.neu.edu>
2019-03-15 03:13:27 -0400

    application start

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "start "
 sudo service tomcat stop
 cd ~
-sudo chmod +x csye6225-0.0.1-SNAPSHOT.jar
 mysql -h csye6225-spring2019.cudakpsmqak8.us-east-1.rds.amazonaws.com -ucsye6225master -p'csye6225password' < sql.sql
-nohup java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.url=jdbc:mysql://csye6225-spring2019.cudakpsmqak8.us-east-1.rds.amazonaws.com/csye6225  --server.port=8080 &
