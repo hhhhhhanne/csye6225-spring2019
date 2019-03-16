ae34449caef15bc6c2fc1cfa91ca0e831571aad4
Xiaohan Zhao <zhao.xiaoh@husky.neu.edu>
2019-03-16 06:20:27 -0400

    kill 8080

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo service tomcat stop
+kill -9 $(lsof -i:8080 |awk '{print $2}' | tail -n 2)
 cd ~
 url=$(cat mysqlsetting.txt | sed -r 's/.*"(.+)".*/\1/')
 mysql -h $url -ucsye6225master -p'csye6225password' < sql.sql
