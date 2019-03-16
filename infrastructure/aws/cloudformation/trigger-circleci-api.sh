3a325171a918cb9ab353e2b8e566feece22c9edb
Ang Li <li.ang2@husky.neu.edu>
2019-03-16 15:04:54 -0400

    delete triggle

--- infrastructure/aws/cloudformation/trigger-circleci-api.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -u aec0589e89999e094deadd9d69f46109c3e824c3 -d build_parameters[CIRCLE_JOB]=build https://circleci.com/api/v1.1/project/github/AngLi2/csye6225-spring2019-ami/tree/assignment5
