23b7d12431590fe23fc70d28920ce477cbbc4585
Xiaohan Zhao <zhao.xiaoh@husky.neu.edu>
2019-03-16 05:50:05 -0400

    mkdir chmod

--- infrastructure/aws/codedeploy/test.sh
+++ infrastructure/aws/codedeploy/test.sh
@@ -3,3 +3,5 @@ sudo service tomcat stop
 cd ~
 url=$(cat mysqlsetting.txt | sed -r 's/.*"(.+)".*/\1/')
 mysql -h $url -ucsye6225master -p'csye6225password' < sql.sql
+sudo mkdir -p ~/webapp/csye6225/target/src/main/resources
+sudo chmod 777 -R ~/webapp/csye6225/target/src/main/resources
