c8e01f5fd03aeb8fe8f2aac89cf5ea170ec73e2f
AngLi2 <31733648+AngLi2@users.noreply.github.com>
2019-03-20 22:25:23 -0400

    add logs to devcontroller

--- webapp/csye6225/src/main/java/edu/neu/xswl/csye6225/controller/DevController.java
+++ webapp/csye6225/src/main/java/edu/neu/xswl/csye6225/controller/DevController.java
@@ -56,12 +56,14 @@ public class DevController {
     @ResponseBody
     public ResponseEntity<?> getAllNote(Principal principal) {
         Users user = userService.getUserByUsername(principal.getName());
+        logger.info("[GET USER NOTES START] Username is: " + user.getUsername());
 
         List<JSONObject> jsonObjectList = new ArrayList<>();
         List<Notes> noteList = noteService.selectByUserId(user.getUserId());
         for (Notes note : noteList)
             jsonObjectList.add(getNoteResponseEntity(new JSONObject(), note));
 
+        logger.info("[GET USER NOTES SUCCESS] Username is: " + user.getUsername());
         return new ResponseEntity<>(jsonObjectList, HttpStatus.OK);
     }
 
@@ -72,22 +74,27 @@ public class DevController {
         JSONObject jsonObject = new JSONObject();
 
         Users user = userService.getUserByUsername(principal.getName());
+        logger.info("[GET NOTE START] Username is: " + user.getUsername());
 
         Notes note = noteService.selectByNoteId(id);
 
         //If note not exist, send BAD_REQUEST
         try {
             note.getNoteId();
+            logger.info("note id is: " + note.getNoteId());
         } catch (Exception e) {
             jsonObject.put("message", "note does not exist");
+            logger.error("note not found, note id is: " + note.getNoteId());
             return new ResponseEntity<>(jsonObject, HttpStatus.NOT_FOUND);
         }
 
         if (!user.getUserId().equals(note.getUserId())) {
             jsonObject.put("message", "user does not match");
+            logger.error("user does not match, note id is: " + note.getNoteId());
             return new ResponseEntity<>(jsonObject, HttpStatus.UNAUTHORIZED);
         }
 
+        logger.info("[GET NOTE SUCCESS] Username is: " + user.getUsername());
         return new ResponseEntity<>(getNoteResponseEntity(jsonObject, note), HttpStatus.OK);
     }
 
@@ -96,6 +103,8 @@ public class DevController {
     @ResponseBody
     public ResponseEntity<?> createNote(@RequestBody(required = false) String jsonNote, Principal principal) {
 
+        logger.info("[POST NOTE START] Username is: " + principal.getName());
+
         JSONObject jsonObject = new JSONObject();
 
         Notes note;
@@ -103,13 +112,16 @@ public class DevController {
             note = JSON.parseObject(jsonNote, Notes.class);
             String noteId = UUID.randomUUID().toString();
             note.setNoteId(noteId);
+            logger.info("note creating.... note id is: " + noteId);
         } catch (Exception e) {
             jsonObject.put("message", "Bad Request");
+            logger.error("Bad Request, json format error");
             return new ResponseEntity<>(jsonObject, HttpStatus.BAD_REQUEST);
         }
 
         if (note.getTitle() == null || note.getTitle().equals("")) {
             jsonObject.put("message", "Title is null");
+            logger.error("Bad Request, title can not be null");
             return new ResponseEntity<>(jsonObject, HttpStatus.BAD_REQUEST);
         }
 
@@ -119,6 +131,7 @@ public class DevController {
 
         note.setUserId(userService.getUserByUsername(principal.getName()).getUserId());
         noteService.addNote(note);
+        logger.info("[POST NOTE SUCCESS] Username is: " + principal.getName());
         return new ResponseEntity<>(getNoteResponseEntity(jsonObject, note), HttpStatus.CREATED);
     }
 
@@ -127,13 +140,17 @@ public class DevController {
     public ResponseEntity<?> updateNote(@PathVariable("id") String id, Principal principal,
                                         @RequestBody(required = false) String jsonNote) {
 
+        logger.info("[PUT NOTE START] Username is: " + principal.getName());
+
         JSONObject jsonObject = new JSONObject();
 
         Notes oldNote = noteService.selectByNoteId(id);
         try {
             oldNote.getNoteId();
+            logger.info("note id is: " + oldNote.getNoteId());
         } catch (Exception e) {
             jsonObject.put("message", "Note Not Exist");
+            logger.error("note not exist, note id is: " + oldNote.getNoteId());
             return new ResponseEntity<>(jsonObject, HttpStatus.BAD_REQUEST);
         }
 
@@ -141,6 +158,7 @@ public class DevController {
 
         if (!user.getUserId().equals(oldNote.getUserId())) {
             jsonObject.put("message", "user does not match");
+            logger.error("user not match");
             return new ResponseEntity<>(jsonObject, HttpStatus.UNAUTHORIZED);
         }
 
@@ -149,13 +167,16 @@ public class DevController {
         try {
             newNote = JSON.parseObject(jsonNote, Notes.class);
             newNote.setNoteId(id);
+            logger.info("new note is creating.....");
         } catch (Exception e) {
             jsonObject.put("message", "Bad Request");
+            logger.error("Bad Request, Json format not correct");
             return new ResponseEntity<>(jsonObject, HttpStatus.BAD_REQUEST);
         }
 
         if (newNote.getTitle() == null || newNote.getTitle().equals("")) {
             jsonObject.put("message", "Title is null");
+            logger.error("Bad Request, title can not null");
             return new ResponseEntity<>(jsonObject, HttpStatus.BAD_REQUEST);
         }
 
@@ -163,6 +184,8 @@ public class DevController {
         newNote.setLastUpdatedOn(current);
         noteService.updateByNoteId(newNote);
         Notes returnNote = noteService.selectByNoteId(id);
+
+        logger.info("[PUT NOTE SUCCESS] Username is: " + principal.getName());
         return new ResponseEntity<>(getNoteResponseEntity(jsonObject, returnNote), HttpStatus.NO_CONTENT);
     }
 
@@ -173,6 +196,7 @@ public class DevController {
         JSONObject jsonObject = new JSONObject();
 
         Users user = userService.getUserByUsername(principal.getName());
+        logger.info("[DELETE NOTE START]");
 
         Notes note;
         List<Attachments> attachmentList;
@@ -181,33 +205,38 @@ public class DevController {
             note = noteService.selectByNoteId(id);
             noteService.selectByNoteId(id).getNoteId();
             attachmentList = attachmentService.selectByNoteId(id);
+            logger.info("note is found.... note id is: " + id);
         } catch (Exception e) {
             jsonObject.put("message", "note does not exist");
+            logger.error("note not exist, note id is: " + id);
             return new ResponseEntity<>(jsonObject, HttpStatus.BAD_REQUEST);
         }
 
         if (!user.getUserId().equals(note.getUserId())) {
             jsonObject.put("message", "user does not match");
+            logger.error("user does not match");
             return new ResponseEntity<>(jsonObject, HttpStatus.UNAUTHORIZED);
         }
 
         //If exist, delete
+        logger.info("deleting note.... note id is: " + id);
         noteService.deleteByNoteId(id);
 
-        for(Attachments attachments: attachmentList){
+        for (Attachments attachments : attachmentList) {
             String path = attachments.getUrl();
             String keyName = S3uploadUtil.getKeyname(path);
-            System.out.println("keyname:" + keyName);
             S3uploadUtil.deleteObject(keyName, S3uploadUtil.bucketName);
         }
 
         //If deleted successfully, send NO_CONTENT
+        logger.info("[DELETE NOTE SUCCESS]");
         return new ResponseEntity<>(jsonObject, HttpStatus.NO_CONTENT);
     }
 
     @GetMapping(value = "/{id}/attachments", produces = "application/json")
     @ResponseBody
     public ResponseEntity<?> getAllAttachments(@PathVariable("id") String id, Principal principal) {
+        logger.info("[GET ATTACHMENTS START]");
 
         List<JSONObject> jsonObjectList = new ArrayList<>();
 
@@ -215,6 +244,7 @@ public class DevController {
 
         Notes note = noteService.selectByNoteId(id);
         if (note == null) {
+            logger.error("bad request, can not find a note");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
@@ -225,9 +255,11 @@ public class DevController {
         } else {
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("message", "user does not match");
+            logger.error("user does not match");
             return new ResponseEntity<>(jsonObject, HttpStatus.UNAUTHORIZED);
         }
 
+        logger.info("[GET ATTACHMENTS SUCCESS]");
         return new ResponseEntity<>(jsonObjectList, HttpStatus.OK);
     }
 
@@ -236,11 +268,13 @@ public class DevController {
     public ResponseEntity<?> postAttachment(@PathVariable("id") String id,
                                             @RequestParam(value = "file", required = false) MultipartFile file,
                                             Principal principal) {
+        logger.info("[POST ATTACHMENTS START]");
         Users user = userService.getUserByUsername(principal.getName());
 
         Notes note = noteService.selectByNoteId(id);
 
         if (file == null || note == null) {
+            logger.error("Bad Request, can not find a file or note");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         Attachments attachment = new Attachments();
@@ -249,43 +283,43 @@ public class DevController {
         String relativePath = System.getProperty("user.dir");
         String filePath = relativePath + folder;
 
-        if(!attachmentService.isURLUnique(fileName)){
+        if (!attachmentService.isURLUnique(fileName)) {
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("message", "url already exist, please rename it or choose a different file");
+            logger.error("Bad Request, url already exist, please rename it or choose a different file");
             return new ResponseEntity<>(jsonObject, HttpStatus.BAD_REQUEST);
         }
 
         String keyName = fileName;
         File fileToUpload = null;
         try {
-//            fileToUpload = transferFile(file, relativePath + folder);
             fileToUpload = convertMultiToFile(file, filePath, fileName);
         } catch (IOException e) {
+            logger.error("Unknown error, convert multipart file not success");
             e.printStackTrace();
         }
-        try {
-            attachment.setAttachmentId(UUID.randomUUID().toString());
-            attachment.setNoteId(id);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+
+        attachment.setAttachmentId(UUID.randomUUID().toString());
+        attachment.setNoteId(id);
 
         if (user.getUserId().equals(note.getUserId())) {
             try {
                 AmazonS3 s3client = new AmazonS3Client(DefaultAWSCredentialsProviderChain.getInstance());
-//                S3uploadUtil.bucketName = s3client.listBuckets().get(0).getName();
                 s3client.putObject(new PutObjectRequest(S3uploadUtil.bucketName, keyName, fileToUpload));
                 fileToUpload.delete();
             } catch (Exception e) {
+                logger.error("error, check s3 status and file to upload path");
                 e.printStackTrace();
             }
             String s3url = S3uploadUtil.getpublicurl(keyName, S3uploadUtil.bucketName);
             attachment.setUrl(s3url);
             attachmentService.addAttachment(attachment);
         } else {
+            logger.error("Unauthorized, user can not attach file to this note");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
+        logger.info("[POST ATTACHMENTS SUCCESS]");
         return new ResponseEntity<>(getAttachmentResponseEntity(new JSONObject(), attachment), HttpStatus.OK);
     }
 
@@ -295,14 +329,17 @@ public class DevController {
                                               @PathVariable("idAttachments") String idAttachments,
                                               @RequestParam(value = "file", required = false) MultipartFile file,
                                               Principal principal) {
+        logger.info("[PUT ATTACHMENTS START]");
         Users user = userService.getUserByUsername(principal.getName());
 
         Notes note = noteService.selectByNoteId(id);
         if (file == null || note == null) {
+            logger.error("Bad Request, can not find a file or note");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         Attachments attachment = attachmentService.selectByAttachmentId(idAttachments);
         if (attachment == null) {
+            logger.error("error, can not find a attachment according to the attachment id");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         String oldPath = attachment.getUrl();
@@ -311,9 +348,10 @@ public class DevController {
         String relativePath = System.getProperty("user.dir");
         String filePath = relativePath + folder;
 
-        if(!attachmentService.isURLUnique(fileName)){
+        if (!attachmentService.isURLUnique(fileName)) {
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("message", "url already exist, please rename it or choose a different file");
+            logger.error("url already exist, please rename it or choose a different file");
             return new ResponseEntity<>(jsonObject, HttpStatus.BAD_REQUEST);
         }
 
@@ -323,9 +361,11 @@ public class DevController {
             try {
                 delete(oldPath);
             } catch (Exception e) {
+                logger.error("Delete file from disk error");
                 e.printStackTrace();
             }
         } else {
+            logger.error("Unauthorized, user can not attach file to this note");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
@@ -337,26 +377,25 @@ public class DevController {
         try {
             fileToUpload = convertMultiToFile(file, filePath, fileName);
         } catch (IOException e) {
+            logger.error("Unknown error, convert multipart file not success");
             e.printStackTrace();
         }
 
-        try {
-            attachment.setUrl(filePath);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+        attachment.setUrl(filePath);
+
         try {
             AmazonS3 s3client = new AmazonS3Client(DefaultAWSCredentialsProviderChain.getInstance());
-//                S3uploadUtil.bucketName = s3client.listBuckets().get(0).getName();
             s3client.putObject(new PutObjectRequest(S3uploadUtil.bucketName, keyName, fileToUpload));
             fileToUpload.delete();
         } catch (Exception e) {
+            logger.error("error, check s3 status and file to upload path");
             e.printStackTrace();
         }
         String s3url = S3uploadUtil.getpublicurl(keyName, S3uploadUtil.bucketName);
         attachment.setUrl(s3url);
         attachmentService.updateByAttachment(attachment);
 
+        logger.info("[PUT ATTACHMENTS SUCCESS]");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
@@ -365,26 +404,30 @@ public class DevController {
     public ResponseEntity<?> deleteAttachment(@PathVariable("id") String id,
                                               @PathVariable("idAttachments") String idAttachments,
                                               Principal principal) {
+        logger.info("[DELETE ATTACHMENTS START]");
         Users user = userService.getUserByUsername(principal.getName());
 
         Notes note = noteService.selectByNoteId(id);
         if (note == null) {
+            logger.error("error, note can not be null");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         Attachments attachments = attachmentService.selectByAttachmentId(idAttachments);
         if (attachments == null) {
+            logger.error("error, attachment can not be null");
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         String oldPath = attachments.getUrl();
         if (user.getUserId().equals(note.getUserId()) && attachments.getNoteId().equals(note.getNoteId())) {
             attachmentService.deleteByAttachmentId(idAttachments);
             String keyName = S3uploadUtil.getKeyname(oldPath);
-            System.out.println("keyname:" + keyName);
             S3uploadUtil.deleteObject(keyName, S3uploadUtil.bucketName);
         } else {
+            logger.error("error, file delete can not success");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
+        logger.info("[DELETE ATTACHMENTS SUCCESS]");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
