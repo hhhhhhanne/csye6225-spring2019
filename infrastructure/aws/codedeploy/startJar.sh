b5758c08944dccc1179b591a91c4786b279cd0c5
Chancelife <liu.changsi@husky.neu.edu>
2019-03-21 14:23:36 -0400

    add log directory

--- infrastructure/aws/codedeploy/startJar.sh
+++ infrastructure/aws/codedeploy/startJar.sh
@@ -2,4 +2,4 @@
 cd ~
 url=$(cat mysqlsetting.txt | sed -r 's/.*"(.+)".*/\1/')
 cd ~/webapp/csye6225/target
-java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.url=jdbc:mysql://$url/csye6225  --server.port=8080 >/dev/null 2>&1 &
+java -jar csye6225-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --spring.datasource.url=jdbc:mysql://$url/csye6225 -Dlogging.file=/var/logs/csye6225.log --server.port=8080 >/dev/null 2>&1 &
