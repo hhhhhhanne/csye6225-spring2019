9cf9c359658a1ba9fa1c5d2429e34d6fe1a362ed
Xiaohan Zhao <zhao.xiaoh@husky.neu.edu>
2019-03-28 01:56:45 -0400

    auth

--- webapp/csye6225/src/main/java/edu/neu/xswl/csye6225/auth/WebSecurityConfig.java
+++ webapp/csye6225/src/main/java/edu/neu/xswl/csye6225/auth/WebSecurityConfig.java
@@ -41,8 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
-                .antMatchers("/reset")
-                .antMatchers("/user/register");
+                .antMatchers("/reset","/user/register");
     }
 
 //    @Bean
