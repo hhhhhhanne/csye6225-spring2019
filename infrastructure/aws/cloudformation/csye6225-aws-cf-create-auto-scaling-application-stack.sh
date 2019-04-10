c39be6e6718d7b5f424c2d11f4ddd5740ce9aedd
Chancelife <liu.changsi@husky.neu.edu>
2019-04-10 11:12:44 -0400

    add additional lb for demo

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -14,6 +14,7 @@ echo "domain name is: "'csye6225-spring2019-'$username'.me'
 
 vpcTag=$network'-csye6225-vpc'
 webappSGTag=$network'-csye6225-WebAppSecurityGroup'
+lbwebSGTag=$network'-csye6225-LBEC2SecurityGroup'
 webappSubnetTag=$network'-csye6225-WebAppSubnet'
 dbSGTag=$network'-csye6225-WebAppDBSecurityGroup'
 dbSubnetTag=$network'-csye6225-DBSubnet'
@@ -23,6 +24,7 @@ imageId=`aws ec2 describe-images --owners self --query "Images[?Description=='Ce
 vpcId=`aws ec2 describe-tags --filters "Name=value,Values=$vpcTag" --query "Tags[0].ResourceId" --output text`
 webappSecurityGroup=`aws ec2 describe-tags --filters "Name=value,Values=$webappSGTag" --query "Tags[0].ResourceId" --output text`
 webappSubnetId=`aws ec2 describe-tags --filters "Name=value,Values=$webappSubnetTag" --query "Tags[0].ResourceId" --output text`
+lbwebSecurityGroup=`aws ec2 describe-tags --filters "Name=value,Values=$lbwebSGTag" --query "Tags[0].ResourceId" --output text`
 dbSecurityGroup=`aws ec2 describe-tags --filters "Name=value,Values=$dbSGTag" --query "Tags[0].ResourceId" --output text`
 dbSubnetId=`aws ec2 describe-tags --filters "Name=value,Values=$dbSubnetTag" --query "Tags[0].ResourceId" --output text`
 dbSubnetGroup=`aws ec2 describe-tags --filters "Name=value,Values=*-dbSubnetGroup" --query "Tags[0].ResourceId" --output text`
@@ -30,8 +32,9 @@ hostedZoneId=`aws route53 list-hosted-zones --query "HostedZones[?Name=='$domain
 serviceRoleArn=`aws iam list-roles --query "Roles[?RoleName=='CodeDeployServiceRole'].Arn" --output text`
 certificateDomainName='csye6225-spring2019-'$username'.me'
 certificateArn=`aws acm list-certificates --query "CertificateSummaryList[?DomainName=='$certificateDomainName'].CertificateArn" --output text`
+echo "your loadbalancer security group is"$lbwebSecurityGroup
 echo "your hostedZoneId is "$hostedZoneId
 echo "your certificate ARN is "$certificateArn
 
-aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-auto-scaling-application.json --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=StackName,ParameterValue=$name ParameterKey=AMIInageId,ParameterValue=$imageId ParameterKey=VPCId,ParameterValue=$vpcId ParameterKey=AppSubNetId,ParameterValue=$webappSubnetId ParameterKey=DbSubNetId,ParameterValue=$dbSubnetId ParameterKey=AppSecurityGroupId,ParameterValue=$webappSecurityGroup ParameterKey=DBSecurityGroupId,ParameterValue=$dbSecurityGroup ParameterKey=UserDomainName,ParameterValue=$domainName ParameterKey=HostedZoneId,ParameterValue=$hostedZoneId ParameterKey=ServiceRoleArn,ParameterValue=$serviceRoleArn ParameterKey=SSLCertificateArn,ParameterValue=$certificateArn
+aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-auto-scaling-application.json --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=StackName,ParameterValue=$name ParameterKey=AMIInageId,ParameterValue=$imageId ParameterKey=VPCId,ParameterValue=$vpcId ParameterKey=AppSubNetId,ParameterValue=$webappSubnetId ParameterKey=DbSubNetId,ParameterValue=$dbSubnetId ParameterKey=AppSecurityGroupId,ParameterValue=$webappSecurityGroup ParameterKey=DBSecurityGroupId,ParameterValue=$dbSecurityGroup ParameterKey=UserDomainName,ParameterValue=$domainName ParameterKey=HostedZoneId,ParameterValue=$hostedZoneId ParameterKey=ServiceRoleArn,ParameterValue=$serviceRoleArn ParameterKey=SSLCertificateArn,ParameterValue=$certificateArn ParameterKey=LBSecurityGroupId,ParameterValue=$lbwebSecurityGroup
 aws cloudformation wait stack-create-complete --stack-name $name
